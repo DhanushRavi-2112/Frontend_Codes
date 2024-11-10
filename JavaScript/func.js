f636fb3cab8de8187c92c4ff8eb4bfd0b2c817dc
DhanushRavi-2112 <dhanushin2112@gmail.com>
2024-11-10 12:26:49 +0530

    feat : "My Frontend Codes"

---
+++ JavaScript/func.js
@@ -0,0 +1,41 @@
+f_actor="Vivek"
+f_movie="John wick"
+f_player="Chris Gail"
+
+function favorite()
+{
+    console.log("Favorite Actor:"+ f_actor)
+    console.log("Favorite Movie"+ f_movie)
+    console.log("Favorite Player:"+ f_player)
+}
+
+function samsung(){
+    console.log("This is Samsung Mobile")
+}
+function iphone(){
+    console.log("This is Iphone Mobile")
+}
+function oppo(){
+    console.log("This is Oppo Mobile")
+}
+function redmi(){
+    console.log("This is Redmi Mobile")
+}
+
+var a=23
+var b=68
+function add(a,b){
+    console.log(a+b)
+}
+
+
+function area(l,b){
+    console.log(l*b)
+}
+
+
+function add(a,b){
+    return a+b
+}
+var total=add(12,23)
+
---
+++ JavaScript/if_else.js
@@ -0,0 +1,12 @@
+var score=110
+if(score<50){
+    console.log("Need Emprovement")
+}
+if(score>50 && score<70)
+{
+    console.log("Good")
+}
+if(score>70 && score<=100)
+{
+    console.log("Excellent!!")
+}
